8394b4999c9d0dffddce7ec2fdfad74bb5610e1a
krlzjmnz <carlos_jmnz@hotmail.com>
2024-11-29 13:50:10 -0600

    Rama con tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["index.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       colors: {
-        brand: "#9A99EA",
-        black: "#060606",
-        gray: "#363636",
-        grayLight: "#EAE9E9",
-      },
+        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
+      }
     },
   },
-  plugins: [],
-};
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
